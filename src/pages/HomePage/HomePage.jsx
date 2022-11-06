c52e5d54f2024245579cba3d435a8a118234189d
Владислав Пимошенко <softboy@macbook-air-vladislav.local>
2022-11-06 18:32:43 +0300

    first commit

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,18 @@
+import { Header } from "../../components/Header/Header"
+import { SVG_ARROW } from "../../const"
+import "./HomePage.css"
+import { HomePageSvgSelector } from "./HomePageSvgSelector"
+
+
+export const HomePage = () => {
+    return (
+        <>  
+            <Header/>
+            <h1 className="title">Добро пожаловать <br/> в <br/> GSU Activity</h1>
+            <div className="swipe-down">
+                <p>Узнать больше</p>
+                <HomePageSvgSelector svg = { SVG_ARROW }/>
+            </div>
+        </>
+    )
+}
